341fd7d4a0ac0b05a71dd04ca7c91014e41ac28c
YanaZelen <104030970+YanaZelen@users.noreply.github.com>
2023-01-15 22:05:56 +0300

    Change String to Player in Ship Class.

--- src/Ship.java
+++ src/Ship.java
@@ -2,27 +2,30 @@
 public class Ship {
     int shipsSize;
     String shipsName;
-    String playersName;
+    Player player;
     static String tender = "Тендер";
     static String schooner = "Шхуна";
     static String brig = "Бриг";
     static String frigate = "Фрегат";
 
 
-    public Ship(String shipsName, int shipsSize, String playersName) {
+    public Ship(String shipsName, int shipsSize, Player player) {
         this.shipsName = shipsName;
         this.shipsSize = shipsSize;
+        this.player = player;
     }
-    public Ship(String shipsName, String playersName) {
+    public Ship(String shipsName, Player player) {
         this.shipsName = shipsName;
+        this.player = player;
     }
-    public Ship(int shipsSize, String playersName) {
+    public Ship(int shipsSize, Player player) {
         this.shipsSize = shipsSize;
+        this.player = player;
     }
 
 
     String getPlayersName() {
-        return playersName;
+        return player.name;
     }
     int getShipsSize() {
         return shipsSize;
